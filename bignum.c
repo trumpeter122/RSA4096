bf85e312b043e242a4f2f108dc19f795ace7cc24
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2026-05-15 19:47:13 +0800

    refactor

--- bignum.c
+++ bignum.c
@@ -17,10 +17,6 @@ static void karatsuba_sub_from(bn_t *a, uint32_t alen, bn_t *b, uint32_t blen);
 static void school_mul(bn_t *a, bn_t *b, bn_t *c, uint32_t digits);
 static void karatsuba_mul(bn_t *a, bn_t *b, bn_t *c, uint32_t digits);
 static uint32_t next_power_of_two(uint32_t digits);
-static bn_t montgomery_n0inv(bn_t n0);
-static void montgomery_r2(bn_t *r2, bn_t *n, uint32_t digits);
-static void montgomery_mul(bn_t *a, bn_t *b, bn_t *c, bn_t *n, uint32_t digits, bn_t n0inv);
-static void reduce_operand(bn_t *a, bn_t *b, bn_t *n, uint32_t digits);
 
 void bn_decode(bn_t *bn, uint32_t digits, uint8_t *hexarr, uint32_t size)
 {
@@ -238,41 +234,13 @@ void bn_mod(bn_t *a, bn_t *b, uint32_t bdigits, bn_t *c, uint32_t cdigits)
 
 void bn_mod_mul(bn_t *a, bn_t *b, bn_t *c, bn_t *d, uint32_t digits)
 {
-    bn_t bb[BN_MAX_DIGITS], cc[BN_MAX_DIGITS], one[BN_MAX_DIGITS];
-    bn_t r2[BN_MAX_DIGITS], bm[BN_MAX_DIGITS], cm[BN_MAX_DIGITS], tm[BN_MAX_DIGITS];
-    bn_t n0inv;
+    bn_t t[2*BN_MAX_DIGITS];
 
-    if(digits == 0 || d[0] == 0 || ((d[0] & 1) == 0)) {
-        bn_t t[2*BN_MAX_DIGITS];
-
-        bn_mul(t, b, c, digits);
-        bn_mod(a, t, 2*digits, d, digits);
-
-        // Clear potentially sensitive information
-        memset((uint8_t *)t, 0, sizeof(t));
-        return;
-    }
-
-    reduce_operand(bb, b, d, digits);
-    reduce_operand(cc, c, d, digits);
-
-    montgomery_r2(r2, d, digits);
-    n0inv = montgomery_n0inv(d[0]);
-    montgomery_mul(bm, bb, r2, d, digits, n0inv);
-    montgomery_mul(cm, cc, r2, d, digits, n0inv);
-    montgomery_mul(tm, bm, cm, d, digits, n0inv);
-
-    BN_ASSIGN_DIGIT(one, 1, digits);
-    montgomery_mul(a, tm, one, d, digits, n0inv);
+    bn_mul(t, b, c, digits);
+    bn_mod(a, t, 2*digits, d, digits);
 
     // Clear potentially sensitive information
-    memset((uint8_t *)bb, 0, sizeof(bb));
-    memset((uint8_t *)cc, 0, sizeof(cc));
-    memset((uint8_t *)one, 0, sizeof(one));
-    memset((uint8_t *)r2, 0, sizeof(r2));
-    memset((uint8_t *)bm, 0, sizeof(bm));
-    memset((uint8_t *)cm, 0, sizeof(cm));
-    memset((uint8_t *)tm, 0, sizeof(tm));
+    memset((uint8_t *)t, 0, sizeof(t));
 }
 
 
@@ -451,76 +419,6 @@ static uint32_t next_power_of_two(uint32_t digits)
     }
 
     return n;
-}
-
-static void reduce_operand(bn_t *a, bn_t *b, bn_t *n, uint32_t digits)
-{
-    if(bn_cmp(b, n, digits) >= 0) {
-        bn_mod(a, b, digits, n, digits);
-    } else {
-        bn_assign(a, b, digits);
-    }
-}
-
-static void montgomery_r2(bn_t *r2, bn_t *n, uint32_t digits)
-{
-    bn_t dividend[2*BN_MAX_DIGITS+1], quotient[2*BN_MAX_DIGITS+1];
-
-    bn_assign_zero(dividend, 2*BN_MAX_DIGITS+1);
-    dividend[2*digits] = 1;
-    bn_div(quotient, r2, dividend, 2*digits + 1, n, digits);
-
-    // Clear potentially sensitive information
-    memset((uint8_t *)dividend, 0, sizeof(dividend));
-    memset((uint8_t *)quotient, 0, sizeof(quotient));
-}
-
-static void montgomery_mul(bn_t *a, bn_t *b, bn_t *c, bn_t *n, uint32_t digits, bn_t n0inv)
-{
-    bn_t t[BN_MAX_DIGITS+1], m, borrow;
-    unsigned __int128 acc, carry;
-    uint32_t i, j;
-
-    bn_assign_zero(t, digits + 1);
-    for(i=0; i<digits; i++) {
-        m = (bn_t)(((dbn_t)t[0] + (dbn_t)b[i] * c[0]) * n0inv);
-        carry = 0;
-
-        for(j=0; j<digits; j++) {
-            acc = (unsigned __int128)t[j] + (unsigned __int128)b[i] * c[j] +
-                  (unsigned __int128)m * n[j] + carry;
-            if(j > 0) {
-                t[j-1] = (bn_t)acc;
-            }
-            carry = acc >> BN_DIGIT_BITS;
-        }
-
-        acc = (unsigned __int128)t[digits] + carry;
-        t[digits-1] = (bn_t)acc;
-        t[digits] = (bn_t)(acc >> BN_DIGIT_BITS);
-    }
-
-    bn_assign(a, t, digits);
-    if(t[digits] || bn_cmp(a, n, digits) >= 0) {
-        borrow = bn_sub(a, a, n, digits);
-        (void)borrow;
-    }
-
-    // Clear potentially sensitive information
-    memset((uint8_t *)t, 0, sizeof(t));
-}
-
-static bn_t montgomery_n0inv(bn_t n0)
-{
-    bn_t x;
-    uint32_t i;
-
-    x = 1;
-    for(i=0; i<5; i++) {
-        x *= 2 - n0 * x;
-    }
-
-    return (bn_t)(0 - x);
 }
 
 static bn_t bn_sub_digit_mul(bn_t *a, bn_t *b, bn_t c, bn_t *d, uint32_t digits)
